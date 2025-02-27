7d577c717184fc03e0e340d95a3a9b10c02534a2
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2025-02-27 03:39:53 +0100

    add unit tests for cycle bits (#25)
    * add unit tests for cycle bits
    
    * Update circuit/openvm/src/gadget/test_utils.rs
    
    Co-authored-by: Han <tinghan0110@gmail.com>
    
    * unreachable
    
    * fix constraints check
    
    * make active_idx private
    
    * trigger ci
    
    * fix clippy
    
    * fix clippy
    
    * clippy nightly
    
    * switch clippy back
    
    * fix ci
    
    * fix ci
    
    * fix ci
    
    * all pin
    
    * test
    
    ---------
    
    Co-authored-by: Han <tinghan0110@gmail.com>

---
+++ circuit/openvm/src/gadget/test_utils.rs
@@ -0,0 +1,52 @@
+use p3_air::AirBuilder;
+use p3_baby_bear::BabyBear;
+use p3_field::FieldAlgebra;
+use p3_matrix::dense::RowMajorMatrix;
+
+pub struct MockAirBuilder {
+    constraints: Vec<BabyBear>,
+}
+
+impl MockAirBuilder {
+    pub fn new() -> Self {
+        Self {
+            constraints: Vec::new(),
+        }
+    }
+
+    pub fn constraints(&self) -> &[BabyBear] {
+        &self.constraints
+    }
+}
+
+impl AirBuilder for MockAirBuilder {
+    type Expr = BabyBear;
+    type Var = BabyBear;
+    type M = RowMajorMatrix<BabyBear>;
+    type F = BabyBear;
+
+    fn main(&self) -> Self::M {
+        unreachable!()
+    }
+
+    fn is_first_row(&self) -> Self::Expr {
+        unreachable!()
+    }
+
+    fn is_last_row(&self) -> Self::Expr {
+        unreachable!()
+    }
+
+    fn is_transition_window(&self, _size: usize) -> Self::Expr {
+        unreachable!()
+    }
+
+    fn assert_zero<I: Into<Self::Expr>>(&mut self, x: I) {
+        let x = x.into();
+        if x == BabyBear::ZERO {
+            self.constraints.push(x);
+        } else {
+            panic!("Assertion failed: x should be zero, but got {x:?}");
+        }
+    }
+}
